1b8773b0184f3890852299ba44888eca5c3c378d
kyhchn <kyaahh69@gmail.com>
2024-01-05 21:14:13 +0700

    feat: add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:apotech/app/core/theme/colors.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'package:get/get.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
   runApp(
     ResponsiveSizer(
       builder: (context, orientation, screenType) => GetMaterialApp(
